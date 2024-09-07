533603faf519895ecb226ab95911a49c4f5f3fbb
Chinemerem Ugbaja <marycynthiaconfirm@gmail.com>
2024-09-07 13:23:24 +0100

    commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import {Navigate, Route, Routes } from 'react-router-dom';
+import Home from './Components/Home';
+
+const App = () => {
+  return (
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/section/:section" element={<Home />} />
+      </Routes>
+  );
+}
+
+export default App
---
+++ src/Components/Home.jsx
@@ -0,0 +1,98 @@
+import { useEffect, useRef, useState } from "react"
+import { NavLink, useParams } from "react-router-dom"
+import About from "./About"
+import Skills from "./Skills"
+import Education from "./Education"
+import Projects from "./Projects"
+import Interests from "./Interests"
+import Contact from "./Contact"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faBars, faTimes } from "@fortawesome/free-solid-svg-icons"
+
+
+const Home = () => {
+  const [mobile, setMobile] = useState(false)
+
+  const {section} = useParams()
+
+  const handleClick = () => {
+    setMobile(!mobile)
+
+
+  }
+
+  const refs = {
+    about: useRef(),
+    contact: useRef(),
+    education: useRef(),
+    interest: useRef(),
+    projects: useRef(),
+    skills: useRef(),
+  };
+
+  useEffect( ()=> {
+    if(section && refs[section]) {
+      refs[section].current.scrollIntoView({behavior: "smooth"})
+    }
+
+  }, [section])
+
+
+  return (
+    <div className="flex justify-between">
+      <div>
+        <div className=" absolute top-0 left-0 md:hidden z-50" >
+          <button className=" float-right p-2 bg-black text-white "onClick ={handleClick}>{mobile? <FontAwesomeIcon icon= {faTimes} className="icon"/>: <FontAwesomeIcon icon={faBars } className="icon"/>}</button>
+        </div>
+        <nav className={`${
+            mobile ? " w-1/2 flex flex-col text-xl font-medium h-screen justify-evenly items-center font-mono fixed bg-zinc-50 :shadow-2xl border-2 border-zinc-100" : "hidden"
+          } flex-col text-xl font-medium h-screen lg:flex md:w-1/5 lg:justify-evenly lg:items-center md:font-mono md:fixed md:z-10 md:bg-zinc-50 md:shadow-2xl md: border-zinc-100`}
+        >
+          <NavLink to ="/section/about">About</NavLink>
+          <NavLink to ="/section/skills">Skills</NavLink>
+          <NavLink to ="/section/education">Education</NavLink>
+          <NavLink to ="/section/projects">Projects</NavLink>
+          <NavLink to ="/section/interest">Interests</NavLink>
+          <NavLink to ="/section/contact">Contact</NavLink>
+        </nav>
+      </div>
+      <div>
+      <section ref={refs.about} style={{height: "50vh"}}>
+        <About />
+      </section>
+      <section ref={refs.skills} style={{height: "100vh"}}>
+        <Skills />
+      </section>
+      <section ref={refs.education} style={{height: "100vh"}}>
+        <Education />
+      </section>
+      <section ref={refs.projects} style={{height: "100vh"}}>
+        <Projects />
+      </section>
+      <section ref={refs.interest} style={{height: "100vh"}}>
+        <Interests />
+      </section>
+      <section ref={refs.contact} style={{height: "100vh"}} >
+        <Contact />
+      </section>
+      </div>
+      
+    </div>
+  )
+}
+
+export default Home
+
+
+//  <nav
+//           className={`${
+//             mobile ? "flex" : "hidden"
+//           } flex-col text-xl font-medium h-screen lg:flex lg:w-1/5 lg:justify-evenly lg:items-center lg:font-mono lg:fixed lg:z-10 lg:bg-zinc-50 lg:shadow-2xl`}
+//         >
+//           <NavLink to="/section/about">About</NavLink>
+//           <NavLink to="/section/skills">Skills</NavLink>
+//           <NavLink to="/section/education">Education</NavLink>
+//           <NavLink to="/section/projects">Projects</NavLink>
+//           <NavLink to="/section/interest">Interests</NavLink>
+//           <NavLink to="/section/contact">Contact</NavLink>
+//         </nav>
