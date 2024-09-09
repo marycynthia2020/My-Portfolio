9cec09822b39497f01d13e3df9517cfb83dde3a6
Chinemerem Ugbaja <marycynthiaconfirm@gmail.com>
2024-09-09 15:26:32 +0100

    commit

--- src/projectsData.jsx
+++ src/projectsData.jsx
@@ -1,9 +1,16 @@
 export const projectData= [
     {
       image: "/movieapp.webp",
-      name: "Movie application",
-      description:  "nnnnnn cevwkewwwwwwwwwwkeeeeeeeeeeeeeeeekkkkkkkkkkkkkkkkkkkkcqwdiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii",
+      name: "Movie Application",
+      description:  "nnnnnn cevwke wwwwwwwwww keeeeeeeeee eeeeeekkkkkkk kkkkkkkkkkkkkcqwdiiiiiiii iiiiiiiiiiiiiiiiiiiii iiiiiiiiiiiiiiiiiiiiiiiii iiiiiiiiiiiiiiiii iiiiiiiiiiiiiiiiiii iiiiiiiiiiiiiiiiiiii iiiiiiiiiiiiiiiiii iiiiiiiiiiiiiii iiiii ii",
       netlify: "https://movie-appllication.netlify.app",
       github: "https://github.com/marycynthia2020/Movie-Application"
-    }
+    },
+    {
+        image: "/portfolio.webp",
+        name: "My Portfolio",
+        description:  "nnnnnn cevwke wwwwwwwwww keeeeeeeeee eeeeeekkkkkkk kkkkkkkkkkkkkcqwdiiiiiiii iiiiiiiiiiiiiiiiiiiii iiiiiiiiiiiiiiiiiiiiiiiii iiiiiiiiiiiiiiiii iiiiiiiiiiiiiiiiiii iiiiiiiiiiiiiiiiiiii iiiiiiiiiiiiiiiiii iiiiiiiiiiiiiii iiiii ii",
+        netlify: "https://chinemeremportfolio.netlify.app",
+        github: "https://github.com/marycynthia2020/My-Portfolio"
+      }
 ]
