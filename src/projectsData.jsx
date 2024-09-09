7b21e03ceca53e4b63659262d349451571c11c53
Chinemerem Ugbaja <marycynthiaconfirm@gmail.com>
2024-09-09 19:19:21 +0100

    commit

--- src/projectsData.jsx
+++ src/projectsData.jsx
@@ -2,15 +2,32 @@ export const projectData= [
     {
       image: "/movieapp.webp",
       name: "Movie Application",
-      description:  "nnnnnn cevwke wwwwwwwwww keeeeeeeeee eeeeeekkkkkkk kkkkkkkkkkkkkcqwdiiiiiiii iiiiiiiiiiiiiiiiiiiii iiiiiiiiiiiiiiiiiiiiiiiii iiiiiiiiiiiiiiiii iiiiiiiiiiiiiiiiiii iiiiiiiiiiiiiiiiiiii iiiiiiiiiiiiiiiiii iiiiiiiiiiiiiii iiiii ii",
+      description: "A responsive React-based application showcasing the latest movies, complete with trailers, detailed information, and a user-friendly interface. Built with modern frontend technologies, the app provides an immersive browsing experience across all devices.",
       netlify: "https://movie-appllication.netlify.app",
       github: "https://github.com/marycynthia2020/Movie-Application"
     },
+
     {
         image: "/portfolio.webp",
         name: "My Portfolio",
-        description:  "nnnnnn cevwke wwwwwwwwww keeeeeeeeee eeeeeekkkkkkk kkkkkkkkkkkkkcqwdiiiiiiii iiiiiiiiiiiiiiiiiiiii iiiiiiiiiiiiiiiiiiiiiiiii iiiiiiiiiiiiiiiii iiiiiiiiiiiiiiiiiii iiiiiiiiiiiiiiiiiiii iiiiiiiiiiiiiiiiii iiiiiiiiiiiiiii iiiii ii",
+        description: "Built with React and Tailwind CSS, this portfolio showcases my projects and growth as a frontend developer. It's a testament to my continuous learning journey, reflecting my passion for exploring new technologies and applying them in real-world projects",
         netlify: "https://chinemeremportfolio.netlify.app",
         github: "https://github.com/marycynthia2020/My-Portfolio"
+      },
+
+      {
+        image: "/profile.webp",
+        name: "Profile Search Application",
+        description:"A dynamic React application designed for organizations to easily filter and search for employees by first and last name. Built with a focus on efficiency and user experience, this tool simplifies employee management and showcases my ability to create practical, real-world solutions.",
+        netlify: "https://employees-profile-search.netlify.app",
+        github: "https://github.com/marycynthia2020/Employees-profile-Search"
+      },
+
+      {
+        image: "/starwars.webp",
+        name: "Starwars",
+        description:"A mini version of a Star Wars-themed site that demonstrates the use of APIs to fetch data and handle various states seamlessly. This project showcases my skills in integrating APIs, managing data, and creating engaging, dynamic user experiences.",
+        netlify: "https://starwars-movie-appp.netlify.app",
+        github: "https://github.com/marycynthia2020/Starwars-Movie-App"
       }
 ]
