3d282601ff9f32fe6515afd43cea4e7e01e7458c
Chinemerem Ugbaja <marycynthiaconfirm@gmail.com>
2024-09-10 12:55:21 +0100

    commit

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -3,8 +3,8 @@ const Contact = () => {
   return (
     <div className="mx-2 h-auto my-14 min-h-screen" >
       <p  className="text-xl font-bold font-mono text-center my-8 ">CONTACT</p>
-      <p className=" text-center font-sans text-xs m-auto leading-relaxed tracking-wide"> CURRENTLY AVAILABLE FOR INTERNSHIPS AND JUNIOR DEVELOPER ROLES,</p>
-      <p className=" text-center font-sans text-xs m-auto leading-relaxed tracking-wide">AND WOULD LOVE TO HEAR FROM YOUR</p>
+      <p className="contact"> CURRENTLY AVAILABLE FOR INTERNSHIPS AND JUNIOR DEVELOPER ROLES,</p>
+      <p className="contact">AND WOULD LOVE TO HEAR FROM YOUR</p>
       <form className="form">
         <div className="label-container">
           <label htmlFor="name" className='label'>Name</label>
@@ -20,6 +20,10 @@ const Contact = () => {
         </div>
         <button type="submit" className="button" >Submit</button>
       </form>
+      <p className="contact">chinemeremugbaja97@gmail.com</p>
+      <p className="contact">Abuja, Nigeria</p>
+      <a href="https://www.linkedin.com/in/chinemerem-ugbaja-439b891a8?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app " className="contact block text-blue-700">LinkedIn</a>
+      
     </div>
   )
 }
